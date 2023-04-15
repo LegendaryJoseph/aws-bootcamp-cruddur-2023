cc0b5838a79380b037a7880a0e0ef1cb7e551b79
LegendaryJoseph <irungujoseph95@gmail.com>
2023-04-15 08:35:53 +0000

    Other minor changes

--- backend-flask/services/notifications_activities.py
+++ backend-flask/services/notifications_activities.py
@@ -1,5 +1,5 @@
 from datetime import datetime, timedelta, timezone
-class HomeActivities:
+class NotificationActivities:
   def run():
     now = datetime.now(timezone.utc).astimezone()
     results = [{
