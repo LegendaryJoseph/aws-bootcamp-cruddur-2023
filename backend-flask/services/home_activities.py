c03865e3aef6e5c48f2d5d74d95807d618a99222
LegendaryJoseph <irungujoseph95@gmail.com>
2023-06-05 08:33:56 +0000

    Working with a query

--- backend-flask/services/home_activities.py
+++ backend-flask/services/home_activities.py
@@ -1,26 +1,17 @@
 from datetime import datetime, timedelta, timezone
 from opentelemetry import trace
 
-from lib.db import pool
+from lib.db import db
 
-# tracer = trace.get_tracer("home.activities")
+#tracer = trace.get_tracer("home.activities")
 
 class HomeActivities:
-  def run(cognito_user_id=None):    
+  def run(cognito_user_id=None):
     #logger.info("HomeActivities")
-    # with tracer.start_as_current_span("home-activites-mock-data"):
-    #   span = trace.get_current_span()
-    #   now = datetime.now(timezone.utc).astimezone()
-    #   span.set_attribute("app.now", now.isoformat())
-      
-      sql = """
-      SELECT * FROM activities
-      """      
-      with pool.connection() as conn:
-        with conn.cursor() as cur:
-          cur.execute(sql)
-          # this will return a tuple
-          # the first field being the data
-          json = cur.fetchall()
-      return json[0]
-      return results
+    #with tracer.start_as_current_span("home-activites-mock-data"):
+    #  span = trace.get_current_span()
+    #  now = datetime.now(timezone.utc).astimezone()
+    #  span.set_attribute("app.now", now.isoformat())
+    sql = db.template('activities','home')
+    results = db.query_array_json(sql)
+    return results
